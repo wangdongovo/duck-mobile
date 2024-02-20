c57dcbc19e8414aa5de93f7bc0e19eb5e0093144
Martina Emerson <wangdongovo@gmail.com>
2024-02-20 20:25:55 +0800

    feat: 添加保存图片功能

--- duck-mobile/ViewController.swift
+++ duck-mobile/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKScriptMessageHandler {
+
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.userContentController.add(self, name: "saveImage")
+
+        webView = WKWebView(frame: self.view.frame, configuration: webConfiguration)
+        self.view.addSubview(webView)
+
+        if let url = URL(string: "http://192.168.220.208:3000") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "saveImage", let base64String = message.body as? String {
+            print("Received message from web: \(base64String)")
+            saveBase64ImageToAlbum(base64String: base64String)
+        }
+    }
 
+    func saveBase64ImageToAlbum(base64String: String) {
+        if let imageData = Data(base64Encoded: base64String) {
+            if let image = UIImage(data: imageData) {
+                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            }
+        }
+    }
 }
 
