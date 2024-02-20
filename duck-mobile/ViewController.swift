ea4c76bceccb89b85c472cd440d25ddd41ed67c8
Martina Emerson <wangdongovo@gmail.com>
2024-02-20 16:48:55 +0800

    Initial Commit

---
+++ duck-mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  duck-mobile
+//
+//  Created by wd on 2024/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
